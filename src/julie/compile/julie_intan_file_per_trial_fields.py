faeab1d0282b332c2bef1df4ac02e3c71da0d6b5
Allen Chen <allenmuhanchen@gmail.com>
2023-10-27 16:35:05 -0400

    filter_spikes_with_epochs

--- src/julie/compile/julie_intan_file_per_trial_fields.py
+++ src/julie/compile/julie_intan_file_per_trial_fields.py
@@ -3,7 +3,6 @@ from clat.intan import spike_file
 from clat.intan.channels import Channel
 from clat.intan.livenotes import map_task_id_to_epochs_with_livenotes
 from clat.intan.marker_channels import epoch_using_marker_channels
-from clat.intan.spike_parsing import filter_spikes_with_epochs
 import os
 import re
 
@@ -26,15 +25,31 @@ class SpikeTimesForChannelsField(TaskField):
 
         stim_epochs_from_markers = epoch_using_marker_channels(digital_in_path)
         epochs_for_task_ids = map_task_id_to_epochs_with_livenotes(notes_path,
-                                                                          stim_epochs_from_markers)
+                                                                   stim_epochs_from_markers)
         spikes_for_channels = filter_spikes_with_epochs(spike_tstamps_for_channels,
-                                                                         epochs_for_task_ids, task_id,
-                                                                         sample_rate=sample_rate)
+                                                        epochs_for_task_ids, task_id,
+                                                        sample_rate=sample_rate)
         return spikes_for_channels
 
 
+def filter_spikes_with_epochs(spike_tstamps_for_channels: dict[Channel, list[float]],
+                              epochs_for_task_ids: dict[int, tuple[int, int]], task_id: int,
+                              sample_rate: float = 30000) -> dict[
+    Channel, list[float]]:
+    filtered_spikes_for_channels = {}
+    epoch = epochs_for_task_ids[task_id]
+    for channel, tstamps in spike_tstamps_for_channels.items():
+        passed_filter = []
+        for spike_tstamp in tstamps:
+            spike_index = int(spike_tstamp * sample_rate)
+            if epoch[0] <= spike_index <= epoch[1]:
+                passed_filter.append(spike_tstamp)
+        filtered_spikes_for_channels[channel] = passed_filter
+    return filtered_spikes_for_channels
+
+
 class EpochStartStopField(TaskField):
-    #TODO: clean up a bit, we're duplicating the epoch retrieval between this and SpikeTimesForChannelsField
+    # TODO: clean up a bit, we're duplicating the epoch retrieval between this and SpikeTimesForChannelsField
     def __init__(self, intan_data_path: str, name: str = "EpochStartStop"):
         super().__init__(name)
         self.intan_data_path = intan_data_path
@@ -54,7 +69,7 @@ class EpochStartStopField(TaskField):
         _, sample_rate = spike_file.fetch_spike_tstamps_from_file(spike_path)
         stim_epochs_from_markers = epoch_using_marker_channels(digital_in_path)
         epochs_for_task_ids = map_task_id_to_epochs_with_livenotes(notes_path,
-                                                                          stim_epochs_from_markers)
+                                                                   stim_epochs_from_markers)
         epoch = epochs_for_task_ids[task_id]
         epoch_start = epoch[0] / sample_rate
         epoch_stop = epoch[1] / sample_rate
