b884386e019b1883b4fb62aafce3d9501976cc37
Julie <hwangjy90@gmail.com>
2024-02-12 16:57:39 -0500

    spike rate applied to graph network

--- src/julie/spike_rate_analysis.py
+++ src/julie/spike_rate_analysis.py
@@ -1,9 +1,12 @@
 import os
 import pandas as pd
+import networkx as nx
 from clat.intan.rhd import load_intan_rhd_format
+from matplotlib import pyplot as plt
 
 from julie.single_channel_analysis import read_pickle, calculate_spike_rate
 from julie.single_unit_analysis import calculate_spike_timestamps
+from julie.social_network_anlaysis.monkeyids import Monkey
 
 
 def main():
@@ -25,17 +28,17 @@ def main():
 
     # average spike rates for each monkey
     unique_monkeys = raw_trial_data['MonkeyName'].dropna().unique().tolist()
-    monkey_specific_spike_rates = []
+    unique_channels = set()
+    for index, row in raw_trial_data.iterrows():
+        unique_channels.update(row['SpikeTimes'].keys())
+
+    avg_spike_rate_by_unit = pd.DataFrame(index=[])
+
     for monkey in unique_monkeys:
         monkey_data = raw_trial_data[raw_trial_data['MonkeyName'] == monkey]
-        unique_channels = set()
-        monkey_specific_spike_rate = []
-        for index, row in raw_trial_data.iterrows():
-            unique_channels.update(row['SpikeTimes'].keys())
-        print(f'Num of Unique Channels: {len(unique_channels)}')
+        monkey_specific_spike_rate = {}
 
         for channel in unique_channels:
-            print(channel)
             spike_rates = []
             for index, row in monkey_data.iterrows():
                 if channel in row['SpikeTimes']:
@@ -45,18 +48,37 @@ def main():
                     print(f"No data for {channel} in row {index}")
 
             avg_spike_rate = sum(spike_rates) / len(spike_rates) if spike_rates else 0
-            print(f'the average spike rate is {avg_spike_rate}')
-            channel_spike_rate = {channel: avg_spike_rate}
-            monkey_specific_spike_rate.append(channel_spike_rate)
-        monkey_specific_spike_rates.append(monkey_specific_spike_rate)
+            monkey_specific_spike_rate[channel] = avg_spike_rate
 
-    result = {'MonkeyNames': unique_monkeys, 'AvgSpikeRates': monkey_specific_spike_rates}
-    result_df = pd.DataFrame(result)
+        # Add monkey-specific spike rates to the DataFrame
+        avg_spike_rate_by_unit[monkey] = pd.Series(monkey_specific_spike_rate)
 
-    print(result_df)
+    print(avg_spike_rate_by_unit)
 
+    G = nx.DiGraph()
+    monkeys = [Monkey.Z_M1.value, Monkey.Z_F1.value, Monkey.Z_F2.value,
+               Monkey.Z_F3.value, Monkey.Z_F4.value, Monkey.Z_F5.value,
+               Monkey.Z_F6.value, Monkey.Z_F7.value, Monkey.Z_J1.value, Monkey.Z_J2.value]
+    G.add_nodes_from(monkeys)
 
+    second_row = avg_spike_rate_by_unit.iloc[1]
+    norm_values = (second_row - second_row.min()) / (second_row.max() - second_row.min())
+    print(f'norm values: {norm_values}')
+    norm_values_dict = norm_values.to_dict()
+    print(f'norm values dict: {norm_values_dict}')
+    set_node_attributes_with_default(G, norm_values_dict, 'spike_rate', default_value=0)
+    print(G.nodes(data=True))
+    # Define a colormap and normalize the values
+    colormap = plt.cm.get_cmap('YlOrBr')
+    attribute = nx.get_node_attributes(G, 'spike_rate')
 
+    # Normalize the spike_rate values to range [0, 1]
+    norm = plt.Normalize(min(attribute.values()), max(attribute.values()))
+    # Map the normalized values to colors using the colormap
+    node_colors = [colormap(norm(attribute[node])) for node in G.nodes()]
+    pos = nx.spring_layout(G)  # Position nodes using the spring layout
+    nx.draw(G, pos, with_labels=True, node_color=node_colors)
+    plt.show()
 
     # spike rate for each picture
     # for index, row in raw_trial_data.iterrows():
@@ -67,6 +89,11 @@ def main():
     #         print(f'spike rate manually calculated: {rate}')
     #         spike_rate = calculate_spike_rate(data, row['EpochStartStop'])
     #         print(f'For unit {unit}, the spike rate is {spike_rate}')
+
+def set_node_attributes_with_default(graph, values_dict, attribute_name, default_value=0):
+    for node in graph.nodes():
+        value = values_dict.get(node, default_value)
+        nx.set_node_attributes(graph, {node: value}, attribute_name)
 
 
 if __name__ == '__main__':
