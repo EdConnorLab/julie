e94c961b8314c7897701d5bce97b9653d97614f3
Julie <hwangjy90@gmail.com>
2024-02-13 15:38:08 -0500

    refactored to include main() function

--- src/julie/social_network_anlaysis/social_data_reader.py
+++ src/julie/social_network_anlaysis/social_data_reader.py
@@ -1,70 +1,122 @@
 import os
 from pathlib import Path
+
+import networkx as nx
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-current_dir = os.getcwd()
-# raw_data_file_name = 'RawData_Organized_on20240208.xlsx'
-raw_data_file_name = 'ZombiesFinalRawData.xlsx'
-file_path = '/Users/julie/General/LocalDocuments/GitHub/Julie/resources/ZombiesFinalRawData.xlsx'
-# file_path = Path(current_dir).parent / 'resources' / raw_data_file_name
-xl = pd.ExcelFile(file_path)
-sheet_names = xl.sheet_names
-# print(sheet_names)
-last_sheet = sheet_names[-1]
-df = xl.parse(last_sheet)
-print(df.columns)
 
-# Rename columns for convenience
-df.rename(columns={'All Occurrence Value':'Behavior'},inplace=True)
-df.rename(columns={'All Occurrence Behavior Social Modifier':'Social Modifier'},inplace=True)
-df.rename(columns={'All Occurrence Space Use Coordinate XY':'Space Use'},inplace=True)
+def main():
+    current_dir = os.getcwd()
+    # raw_data_file_name = 'RawData_Organized_on20240208.xlsx'
+    raw_data_file_name = 'ZombiesFinalRawData.xlsx'
+    # file_path = '/Users/julie/General/LocalDocuments/GitHub/Julie/resources/ZombiesFinalRawData.xlsx'
+    file_path = Path(current_dir).parent.parent.parent / 'resources' / raw_data_file_name
+    xl = pd.ExcelFile(file_path)
+    sheet_names = xl.sheet_names
+    # print(sheet_names)
+    last_sheet = sheet_names[-1]
+    df = xl.parse(last_sheet)
+    print(df.columns)
 
-# Rename the last column to Time
-df.columns = [*df.columns[:-1], 'Time']
+    # Rename columns for convenience
+    df.rename(columns={'All Occurrence Value': 'Behavior'}, inplace=True)
+    df.rename(columns={'All Occurrence Behavior Social Modifier': 'Social Modifier'}, inplace=True)
+    df.rename(columns={'All Occurrence Space Use Coordinate XY': 'Space Use'}, inplace=True)
 
-# Combine Year, Month, Day columns into VideoDate
-df['VideoDate'] = df.iloc[:,-4].astype(str).str.zfill(2) + df.iloc[:,-3].astype(str).str.zfill(2) + df.iloc[:,-2].astype(str).str.zfill(2)
-#print(df.columns)
+    # Rename the last column to Time
+    df.columns = [*df.columns[:-1], 'Time']
+
+    # Combine Year, Month, Day columns into VideoDate
+    df['VideoDate'] = df.iloc[:, -4].astype(str).str.zfill(2) + df.iloc[:, -3].astype(str).str.zfill(2) + df.iloc[:,
+                                                                                                          -2].astype(
+        str).str.zfill(2)
+    # print(df.columns)
+
+    extracted_df = df[['Observer', 'Focal Name', 'Behavior', 'Social Modifier', 'Space Use', 'VideoDate', 'Time']]
+
+    print(extracted_df['VideoDate'].unique())
+    print(f"{extracted_df['VideoDate'].nunique()} days of recording")
+
+    validate_number_of_monkeys(df)
+    validate_number_of_interval_datapoints(df)
+
+    ''' GET ALL AGONISTIC BEHAVIORS '''
+    agonistic = extract_pairwise_interactions(df, 'agonistic')
+    # Splitting values with comma and creating new rows
+    new_rows = []
+    for index, row in agonistic.iterrows():
+        if ',' in row['Social Modifier']:
+            modifiers = row['Social Modifier'].split(',')
+            for modifier in modifiers:
+                new_rows.append({col: row[col] for col in agonistic.columns})
+                new_rows[-1]['Social Modifier'] = modifier
+        else:
+            new_rows.append({col: row[col] for col in agonistic.columns})
+    new_df = pd.DataFrame(new_rows)
+
+    edge_weights = new_df.groupby(['Focal Name', 'Social Modifier']).size().reset_index(name='weight')
+    print(edge_weights)
+    G = nx.from_pandas_edgelist(edge_weights, source='Focal Name', target='Social Modifier', edge_attr='weight',
+                                create_using=nx.DiGraph)
+    adj_matrix = nx.to_numpy_array(G, nodelist=sorted(G.nodes()))
+    print(adj_matrix)
+    adj_df = pd.DataFrame(adj_matrix, index=sorted(G.nodes()), columns=sorted(G.nodes()))
+
+    # Extract weights for each edge
+    weights = [d['weight'] for _, _, d in G.edges(data=True)]
+
+    # Visualize the graph with a different layout and edge color
+    plt.figure(figsize=(10, 8))
+    pos = nx.spring_layout(G, seed=42)  # positions for all nodes, seed for reproducibility
+    nx.draw(G, pos, with_labels=True, node_size=700, node_color="skyblue", font_size=12, arrowsize=20, width=weights,
+            edge_color='darkblue')
+    plt.show()
+
+def extract_pairwise_interactions(df, social_interaction_type):
+    if social_interaction_type.lower() == 'agonistic':
+        subset = df[(df['Behavior Abbrev'] == 'AOA') | (df['Behavior Abbrev'] == 'IAG')]
+    elif social_interaction_type.lower() == 'affiliative':
+        subset = df[(df['Behavior Abbrev'] == 'IAF')]
+    elif social_interaction_type.lower() == 'submissive':
+        subset = df[(df['Behavior Abbrev'] == 'AOS') | (df['Behavior Abbrev'] == 'ISU')]
+    else:
+        raise ValueError("Invalid social interaction type. Please provide 'agonistic', 'affiliative', or 'submissive'.")
+    subset = subset[['Focal Name', 'Social Modifier']]
+    subset = subset.dropna(subset=['Social Modifier']) # remove all nan
+
+    return subset
+
+def validate_number_of_monkeys(df):
+    # For dates before 06/13/2022, 10 monkeys
+    # after 06/13/2022, 8 monkeys
+    grouped = df.groupby('VideoDate')['Focal Name'].agg(['nunique', 'unique']).reset_index()
+
+    for index, row in grouped.iterrows():
+        video_date = row['VideoDate']
+        # unique_values = ', '.join(row['unique'])
+        count = row['nunique']
+        print(f"VideoDate: {video_date}, # of unique monkeys: {count}")
+
+def validate_number_of_interval_datapoints(df):
+
+    ''' CHECK NUMBER OF INTERVAL DATA '''
+    df['Behavior Abbrev'] = df['Behavior'].str[:4].str.replace(' ', '')
+
+    # Create a mask to check if 'Behavior Abbrev' starts with 'I'
+    mask = df['Behavior Abbrev'].str.startswith('I')
+
+    # Filter the DataFrame to include only rows where 'Behavior Abbrev' starts with 'I'
+    interval = df[mask].groupby('VideoDate')['Behavior Abbrev'].count().reset_index()
+    filtered = interval[(interval['Behavior Abbrev'] != 120) & (interval['Behavior Abbrev'] != 96)]
+    print(filtered)
+
+    # Group by 'VideoDate' and 'Focal Name' and count the occurrences
+    result = df[mask].groupby(['VideoDate', 'Focal Name']).size().reset_index(name='Count')
+    filtered_result = result[(result['Count'] != 12)]
+    print(filtered_result)
 
 
-extracted_df = df[['Observer','Focal Name', 'Behavior', 'Social Modifier', 'Space Use', 'VideoDate', 'Time']]
-
-print(extracted_df['VideoDate'].unique())
-print(f"{extracted_df['VideoDate'].nunique()} days of recording")
-
-''' CHECK NUMBER OF MONKEYS '''
-# For dates before 06/13/2022, 10 monkeys
-# after 06/13/2022, 8 monkeys
-grouped = df.groupby('VideoDate')['Focal Name'].agg(['nunique', 'unique']).reset_index()
-
-for index, row in grouped.iterrows():
-    video_date = row['VideoDate']
-    # unique_values = ', '.join(row['unique'])
-    count = row['nunique']
-    print(f"VideoDate: {video_date}, # of unique monkeys: {count}")
-
-# For each unique date, there should be
-
-''' CHECK NUMBER OF INTERVAL DATA '''
-df['Behavior Abbrev'] = df['Behavior'].str[:4].str.replace(' ', '')
-
-# Create a mask to check if 'Behavior Abbrev' starts with 'I'
-mask = df['Behavior Abbrev'].str.startswith('I')
-
-# Filter the DataFrame to include only rows where 'Behavior Abbrev' starts with 'I'
-interval = df[mask].groupby('VideoDate')['Behavior Abbrev'].count().reset_index()
-filtered = interval[(interval['Behavior Abbrev'] != 120) & (interval['Behavior Abbrev'] != 96)]
-print(filtered)
-
-# Group by 'VideoDate' and 'Focal Name' and count the occurrences
-result = df[mask].groupby(['VideoDate', 'Focal Name']).size().reset_index(name='Count')
-filtered_result = result[(result['Count'] != 12)]
-print(filtered_result)
-
-
-''' GET ALL AGONISTIC BEHAVIORS '''
-agonistic = df[(df['Behavior Abbrev'] == 'AOA') | (df['Behavior Abbrev'] == 'IAG')]
-agonistic = agonistic[['Focal Name', 'Social Modifier']]
-print(agonistic['Social Modifier'].unique())
+if __name__ == '__main__':
+    main()
