0f9e0223a5e53889653b725e39983f57a938edf3
Julie <hwangjy90@gmail.com>
2024-02-14 14:37:58 -0500

    clean up social_data_reader.py

--- src/julie/social_network_anlaysis/social_data_reader.py
+++ src/julie/social_network_anlaysis/social_data_reader.py
@@ -5,49 +5,25 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-def main():
+
+def read_social_data_and_validate():
+    # Load raw data file
     current_dir = os.getcwd()
     raw_data_file_name = 'ZombiesFinalRawData.xlsx'
-    # file_path = '/Users/julie/General/LocalDocuments/GitHub/Julie/resources/ZombiesFinalRawData.xlsx'
     file_path = Path(current_dir).parent.parent.parent / 'resources' / raw_data_file_name
-    df = read_social_raw_data(file_path)
-    extracted_df = clean_social_raw_data(df)
+    raw_social_data = read_raw_social_data(file_path)
 
-    # Validate
+    # Clean raw data
+    social_data = clean_raw_social_data(raw_social_data)
 
     # Check number of monkeys recorded for each day
-    try:
-        validate_number_of_monkeys(df)
-        print("Valid number of Monkeys")
-    except ValueError as e:
-        print("Validation failed: ", e)
+    validate_number_of_monkeys(social_data)
 
     # Check number of interval datapoints for each day
-    validate_number_of_interval_datapoints(df)
+    validate_number_of_interval_datapoints(social_data)
 
-    ''' GET ALL AGONISTIC BEHAVIORS '''
-    agonistic = extract_pairwise_interactions(df, 'agonistic')
-    edge_weights = generate_weights_from_pairwise_interactions(agonistic)
 
-    edge_weights.to_csv('edge_weights.csv', index=False)
-
-    G = nx.from_pandas_edgelist(edge_weights, source='Focal Name', target='Social Modifier', edge_attr='weight',
-                                    create_using=nx.DiGraph)
-    adj_matrix = nx.to_numpy_array(G, nodelist=sorted(G.nodes()))
-    normalized_matrix = adj_matrix / np.sum(adj_matrix)
-    print(adj_matrix)
-    adj_df = pd.DataFrame(normalized_matrix, index=sorted(G.nodes()), columns=sorted(G.nodes()))
-    print(G.edges(data=True))
-    # Extract weights for each edge
-    weights = [d['weight'] for _, _, d in G.edges(data=True)]
-
-    # Visualize the graph with a different layout and edge color
-    plt.figure(figsize=(10, 8))
-    pos = nx.spring_layout(G, seed=42)  # positions for all nodes, seed for reproducibility
-    nx.draw(G, pos, with_labels=True, node_size=700, font_size=10, arrowsize=15, width=weights,
-            edge_color='darkblue')
-    plt.show()
-def read_social_raw_data(filepath):
+def read_raw_social_data(filepath):
     xl = pd.ExcelFile(filepath)
     sheet_names = xl.sheet_names
     # print(sheet_names)
@@ -56,26 +32,88 @@ def read_social_raw_data(filepath):
     # print(df.columns)
     return df
 
-def clean_social_raw_data(df):
+
+def clean_raw_social_data(raw_social_data):
     # Rename columns for convenience
-    df.rename(columns={'All Occurrence Value': 'Behavior'}, inplace=True)
-    df.rename(columns={'All Occurrence Behavior Social Modifier': 'Social Modifier'}, inplace=True)
-    df.rename(columns={'All Occurrence Space Use Coordinate XY': 'Space Use'}, inplace=True)
+    raw_social_data.rename(columns={'All Occurrence Value': 'Behavior'}, inplace=True)
+    raw_social_data.rename(columns={'All Occurrence Behavior Social Modifier': 'Social Modifier'}, inplace=True)
+    raw_social_data.rename(columns={'All Occurrence Space Use Coordinate XY': 'Space Use'}, inplace=True)
 
     # Rename the last column to Time
-    df.columns = [*df.columns[:-1], 'Time']
+    raw_social_data.columns = [*raw_social_data.columns[:-1], 'Time']
 
     # Combine Year, Month, Day columns into VideoDate
-    df['VideoDate'] = (df.iloc[:, -4].astype(str).str.zfill(2) + df.iloc[:, -3].astype(str).str.zfill(2)
-                       + df.iloc[:,-2].astype(str).str.zfill(2))
+    raw_social_data['VideoDate'] = (
+                raw_social_data.iloc[:, -4].astype(str).str.zfill(2) + raw_social_data.iloc[:, -3].astype(
+            str).str.zfill(2)
+                + raw_social_data.iloc[:, -2].astype(str).str.zfill(2))
     # print(df.columns)
 
-    extracted_df = df[['Observer', 'Focal Name', 'Behavior', 'Social Modifier', 'Space Use', 'VideoDate', 'Time']]
+    social_data = raw_social_data[
+        ['Observer', 'Focal Name', 'Behavior', 'Social Modifier', 'Space Use', 'VideoDate', 'Time']].copy()
+    print(social_data)
+    return social_data
 
-    # print(extracted_df['VideoDate'].unique())
-    print(f"{extracted_df['VideoDate'].nunique()} days of recording")
+def validate_number_of_monkeys(social_data):
+    # For dates before 06/13/2022, 10 monkeys
+    # after 06/13/2022, 8 monkeys
+    # exception: 05/19/2022, 8 monkeys
+    grouped = social_data.groupby('VideoDate')['Focal Name'].agg(['nunique', 'unique']).reset_index()
+
+    for index, row in grouped.iterrows():
+        video_date = row['VideoDate']
+        # unique_values = ', '.join(row['unique'])
+        count = row['nunique']
+        # print(f"VideoDate: {video_date}, # of unique monkeys: {count}")
+
+        expected_count = 10 if video_date < '20220613' else 8
+        if video_date == '20220519':
+            expected_count = 8
+        else:
+            expected_count = 10 if video_date < '20220613' else 8
+
+        if count != expected_count:
+            raise ValueError(
+                f"Unexpected number of monkeys ({count}) observed on {video_date}. Expected: {expected_count} monkeys.")
+        else:
+            print("Validation passed! Valid number of monkeys for all dates :)")
+
+def validate_number_of_interval_datapoints(social_data):
+    if 'Behavior Abbrev' not in social_data.columns:
+        social_data['Behavior Abbrev'] = social_data['Behavior'].str[:4].str.replace(' ', '')
+
+    ''' CHECK NUMBER OF INTERVAL DATA '''
+    # Create a mask to check if 'Behavior Abbrev' starts with 'I'
+    mask = social_data['Behavior Abbrev'].str.startswith('I')
+
+    # Filter the DataFrame to include only rows where 'Behavior Abbrev' starts with 'I'
+    interval = social_data[mask].groupby('VideoDate')['Behavior Abbrev'].count().reset_index()
+    filtered = interval[(interval['Behavior Abbrev'] != 120) & (interval['Behavior Abbrev'] != 96)]
+    result = social_data[mask].groupby(['VideoDate', 'Focal Name']).size().reset_index(name='Count')
+    filtered_result = result[(result['Count'] != 12)]
+
+    if filtered.empty:
+        print("Validation passed! Valid number of interval datapoints for all dates :)")
+    else:
+        raise ValueError(f'Invalid number of interval datapoints! : {filtered}')
+        raise ValueError(f'Monkey specific interval datapoint count: {filtered_result}')
 
 
+def extract_pairwise_interactions(social_data, social_interaction_type):
+    social_data['Behavior Abbrev'] = social_data['Behavior'].str[:4].str.replace(' ', '')
+
+    if social_interaction_type.lower() == 'agonistic':
+        subset = social_data[(social_data['Behavior Abbrev'] == 'AOA') | (social_data['Behavior Abbrev'] == 'IAG')]
+    elif social_interaction_type.lower() == 'affiliative':
+        subset = social_data[(social_data['Behavior Abbrev'] == 'IAF')]
+    elif social_interaction_type.lower() == 'submissive':
+        subset = social_data[(social_data['Behavior Abbrev'] == 'AOS') | (social_data['Behavior Abbrev'] == 'ISU')]
+    else:
+        raise ValueError("Invalid social interaction type. Please provide 'agonistic', 'affiliative', or 'submissive'.")
+    subset = subset[['Focal Name', 'Social Modifier']]
+    subset = subset.dropna(subset=['Social Modifier'])  # remove all nan
+
+    return subset
 
 def generate_weights_from_pairwise_interactions(interaction_df):
     # Splitting values with comma and creating new rows
@@ -92,60 +130,5 @@ def generate_weights_from_pairwise_interactions(interaction_df):
     edge_weights = pd.DataFrame(new_rows).groupby(['Focal Name', 'Social Modifier']).size().reset_index(name='weight')
     return edge_weights
 
-def extract_pairwise_interactions(df, social_interaction_type):
-    if social_interaction_type.lower() == 'agonistic':
-        subset = df[(df['Behavior Abbrev'] == 'AOA') | (df['Behavior Abbrev'] == 'IAG')]
-    elif social_interaction_type.lower() == 'affiliative':
-        subset = df[(df['Behavior Abbrev'] == 'IAF')]
-    elif social_interaction_type.lower() == 'submissive':
-        subset = df[(df['Behavior Abbrev'] == 'AOS') | (df['Behavior Abbrev'] == 'ISU')]
-    else:
-        raise ValueError("Invalid social interaction type. Please provide 'agonistic', 'affiliative', or 'submissive'.")
-    subset = subset[['Focal Name', 'Social Modifier']]
-    subset = subset.dropna(subset=['Social Modifier']) # remove all nan
-
-    return subset
-
-def validate_number_of_monkeys(df):
-    # For dates before 06/13/2022, 10 monkeys
-    # after 06/13/2022, 8 monkeys
-    # exception: 05/19/2022, 8 monkeys
-    grouped = df.groupby('VideoDate')['Focal Name'].agg(['nunique', 'unique']).reset_index()
-
-    for index, row in grouped.iterrows():
-        video_date = row['VideoDate']
-        # unique_values = ', '.join(row['unique'])
-        count = row['nunique']
-        # print(f"VideoDate: {video_date}, # of unique monkeys: {count}")
-
-        expected_count = 10 if video_date < '20220613' else 8
-        if video_date == '20220519':
-            expected_count = 8
-        else:
-            expected_count = 10 if video_date < '20220613' else 8
-        if count != expected_count:
-            raise ValueError(
-                f"Unexpected number of monkeys ({count}) observed on {video_date}. Expected: {expected_count} monkeys.")
-
-def validate_number_of_interval_datapoints(df):
-
-    ''' CHECK NUMBER OF INTERVAL DATA '''
-    df['Behavior Abbrev'] = df['Behavior'].str[:4].str.replace(' ', '')
-
-    # Create a mask to check if 'Behavior Abbrev' starts with 'I'
-    mask = df['Behavior Abbrev'].str.startswith('I')
-
-    # Filter the DataFrame to include only rows where 'Behavior Abbrev' starts with 'I'
-    interval = df[mask].groupby('VideoDate')['Behavior Abbrev'].count().reset_index()
-    filtered = interval[(interval['Behavior Abbrev'] != 120) & (interval['Behavior Abbrev'] != 96)]
-    print(f'Invalid number of interval datapoints! : {filtered}')
-
-    # Group by 'VideoDate' and 'Focal Name' and count the occurrences
-    result = df[mask].groupby(['VideoDate', 'Focal Name']).size().reset_index(name='Count')
-    filtered_result = result[(result['Count'] != 12)]
-    print(f'Monkey specific interval datapoint count: {filtered_result}')
-
-
-
 if __name__ == '__main__':
-    main()
+    read_social_data_and_validate()
