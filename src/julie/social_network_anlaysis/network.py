4c616345336473512125a1a92777ce4974cbb935
Julie <hwangjy90@gmail.com>
2024-02-14 15:09:57 -0500

    added create basic social graph function

--- src/julie/social_network_anlaysis/network.py
+++ src/julie/social_network_anlaysis/network.py
@@ -8,7 +8,7 @@ from monkeyids import Monkey
 
 def create_digraph_with_edge_weights(edge_weights):
     G = nx.from_pandas_edgelist(edge_weights, source='Focal Name', target='Social Modifier', edge_attr='weight',
-                                    create_using=nx.DiGraph)
+                                create_using=nx.DiGraph)
     adj_matrix = nx.to_numpy_array(G, nodelist=sorted(G.nodes()))
     normalized_matrix = adj_matrix / np.sum(adj_matrix)
     print(adj_matrix)
@@ -18,13 +18,41 @@ def create_digraph_with_edge_weights(edge_weights):
     weights = [d['weight'] for _, _, d in G.edges(data=True)]
     return G, adj_matrix, weights
 
+
 def create_graph_with_edge_weights(edge_weights):
     pass
 
-def create_digraph_for_zombies():
-    G = nx.DiGraph()
-    zombies = [Monkey.Z_M1.value, Monkey.Z_F1.value, Monkey.Z_F2.value,
-               Monkey.Z_F3.value, Monkey.Z_F4.value, Monkey.Z_F5.value,
-               Monkey.Z_F6.value, Monkey.Z_F7.value, Monkey.Z_J1.value, Monkey.Z_J2.value]
-    G.add_nodes_from(zombies)
+
+def create_social_graph_for(graph_type, monkey_group):
+
+    if graph_type == 'digraph':
+        G = nx.DiGraph()
+    elif graph_type == 'graph'
+        G = nx.Graph()
+    else:
+        raise ValueError("Invalid graph type. Please provide 'digraph' or 'graph' as the type.")
+
+    if monkey_group.lower() == 'zombies':
+        zombies = [Monkey.Z_M1.value, Monkey.Z_F1.value, Monkey.Z_F2.value,
+                   Monkey.Z_F3.value, Monkey.Z_F4.value, Monkey.Z_F5.value,
+                   Monkey.Z_F6.value, Monkey.Z_F7.value, Monkey.Z_J1.value, Monkey.Z_J2.value]
+        G.add_nodes_from(zombies)
+    elif monkey_group.lower() == 'bestfrans':
+        bestfrans = [Monkey.B_M1.value, Monkey.B_F1.value, Monkey.B_F2.value, Monkey.B_F3.value, Monkey.B_J1.value]
+        G.add_nodes_from(bestfrans)
+    elif monkey_group.lower() == 'instigators':
+        instigators = [Monkey.I_M1.value, Monkey.I_F1.value, Monkey.I_F2.value, Monkey.I_F3.value, Monkey.I_F4.value,
+                       Monkey.I_F5.value, Monkey.I_F6.value, Monkey.I_F7.value, Monkey.I_F8.value, Monkey.I_F9.value,
+                       Monkey.I_J1.value, Monkey.I_J2.value, Monkey.I_J3.value, Monkey.I_J4.value]
+        G.add_nodes_from(instigators)
+    elif monkey_group.lower() == 'strangerthings':
+        strangerthings = [Monkey.S_M1.value, Monkey.S_F1.value, Monkey.S_F2.value, Monkey.S_F3.value, Monkey.S_F4.value,
+                          Monkey.S_J1.value, Monkey.S_J2.value, Monkey.S_J3.value, Monkey.S_J4.value, Monkey.S_J5.value,
+                          Monkey.S_J6.value, Monkey]
+        G.add_nodes_from(strangerthings)
+    else:
+        raise ValueError("Invalid monkey group name. Please provide 'zombies', 'bestfrans', 'instigators', "
+                         "or 'strangerthings'.")
+
     return G
+
