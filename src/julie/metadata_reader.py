d541178104271173416d028125378bf4f614352e
Julie <hwangjy90@gmail.com>
2024-03-04 10:57:05 -0500

    -

---
+++ src/julie/metadata_reader.py
@@ -0,0 +1,63 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+
+
+class RecordingMetadataReader:
+
+    def __init__(self):
+        raw_data_file_name = 'Cortana_Recording_Metadata.xlsx'
+        dir = '/home/connorlab/Documents/GitHub/Julie/resources'
+        file_path = Path(os.path.join(dir, raw_data_file_name))
+        xl = pd.ExcelFile(file_path)
+        channels_tab = xl.parse('Channels')
+        locations_tab = xl.parse('Location')
+        recording_metadata = pd.merge(channels_tab, locations_tab, on=['Date', 'Round No.'])
+        self.xl = xl
+        self.recording_metadata = recording_metadata
+
+    def get_metadata(self):
+        return self.recording_metadata
+
+    def get_valid_channels(self, date, round_number) -> list:
+        matching_round = self.recording_metadata[
+            (self.recording_metadata['Date'] == date) & (self.recording_metadata['Round No.'] == round_number)]
+        channels = matching_round['Channels1'].apply(lambda x: [int(i.strip()) for i in x.split(',')]).tolist()
+        formatted_channels = ['Channel.C_{:03.0f}'.format(channel) for channel in channels[0]]
+        return formatted_channels
+
+    def get_pickle_filenames_for_specific_date(self, date):
+        matching_date = self.recording_metadata[(self.recording_metadata['Date'] == date)]
+        filenames = matching_date['Pickle File Name']
+        return list(filenames)
+
+    def get_pickle_filename_for_specific_round(self, date, round_number):
+        matching_round = self.recording_metadata[
+            (self.recording_metadata['Date'] == date) & (self.recording_metadata['Round No.'] == round_number)]
+        filename = matching_round['Pickle File Name'].iloc[0]
+        return str(filename)
+
+    def get_intan_folder_names_for_specific_date(self, date):
+        matching_date = self.recording_metadata[(self.recording_metadata['Date'] == date)]
+        folder_names = matching_date['Folder Name']
+        return list(folder_names)
+
+    def get_intan_folder_name_for_specific_round(self, date, round_number):
+        matching_round = self.recording_metadata[
+            (self.recording_metadata['Date'] == date) & (self.recording_metadata['Round No.'] == round_number)]
+        print(matching_round)
+        folder_name = matching_round['Folder Name']
+        print(folder_name)
+        return str(folder_name)
+
+
+if __name__ == "__main__":
+    reader = RecordingMetadataReader()
+    metadata = reader.recording_metadata
+    filename = reader.get_pickle_filename_for_specific_round("2023-10-27", 1)
+    ER_data = metadata[metadata['Location'] == 'ER']
+    AMG_data = metadata[metadata['Location'] == 'Amygdala']
+    # print("JH 12: \n", ER_data)
+    # print("JH 32192: \n", AMG_data)
+    # get_valid_channels("10-10-2023", "1696957915096002_231010_131155")
--- src/julie/spike_rate_analysis.py
+++ src/julie/spike_rate_analysis.py
@@ -4,45 +4,47 @@ from pathlib import Path
 import pandas as pd
 import networkx as nx
 from clat.intan.rhd import load_intan_rhd_format
-from clat.intan.channels import Channel
-from matplotlib import pyplot as plt
 
-from julie.single_channel_analysis import read_pickle, calculate_spike_rate, extract_target_channel_data
+from julie.single_channel_analysis import read_pickle, calculate_spike_rate
 from julie.single_unit_analysis import calculate_spike_timestamps
-from neural_metadata_reader.metadata_reader import get_valid_channels
+from metadata_reader import RecordingMetadataReader
 
 
-def compute_average_spike_rates(date, round):
+def compute_average_spike_rates(date, round_number):
+    metadata_reader = RecordingMetadataReader()
+
+    pickle_filename = metadata_reader.get_pickle_filename_for_specific_round(date, round_number) + ".pk1"
+    compiled_dir = (Path(__file__).parent.parent.parent / 'compiled').resolve()
+    pickle_filepath = os.path.join(compiled_dir, pickle_filename)
+    raw_trial_data = read_pickle(pickle_filepath)
+
+    intan_dir = metadata_reader.get_intan_folder_name_for_specific_round(date, round_number)
     cortana_path = "/home/connorlab/Documents/IntanData/Cortana"
-    round_path = Path(os.path.join(cortana_path, date, round))
-    script_dir = Path(__file__).parent
-    compiled_dir = (script_dir / '..' / '..' / 'compiled').resolve()
-    matching_files = list(compiled_dir.glob(f"*{round}*"))
-    raw_trial_data = read_pickle(matching_files[0])
-    print("raw trial data printed here: ")
-    print(raw_trial_data)
+    round_path = Path(os.path.join(cortana_path, date, intan_dir))
 
     # Check if the experimental round is sorted
     sorted = round_path / 'sorted_spikes.pkl'
     if sorted.exists():
-        print("This round contains sorted spikes")
         sorted_data = read_sorted_data(round_path)
         channels_with_units = set()
         channels_with_units.update(sorted_data['SpikeTimes'][0].keys())
         sorted_channels = {channel.split('_Unit')[0] for channel in channels_with_units}
-    # TODO: get the valid channels from metadata
-    valid_channels = set(get_valid_channels(date, round)) - sorted_channels
-    sorted_data_spike_rates = compute_spike_rates_per_channel_per_monkey_for_all_channels(sorted_data)
-    raw_data_spike_rates = compute_spike_rates_per_channel_per_monkey(raw_trial_data, valid_channels)
-    print(sorted_data_spike_rates)
-    print(raw_data_spike_rates)
-    average_spike_rates = pd.concat([sorted_data_spike_rates, raw_data_spike_rates])
-    print('-----------------------combined--------------------')
+        valid_channels = set(metadata_reader.get_valid_channels(date, round_number)) - sorted_channels
+        sorted_data_spike_rates = compute_spike_rates_per_channel_per_monkey_for_all_channels(sorted_data)
+        raw_data_spike_rates = compute_spike_rates_per_channel_per_monkey(raw_trial_data, valid_channels)
+        average_spike_rates = pd.concat([sorted_data_spike_rates, raw_data_spike_rates])
+    else:
+        valid_channels = set(metadata_reader.get_valid_channels(date, round_number))
+        raw_data_spike_rates = compute_spike_rates_per_channel_per_monkey(raw_trial_data, valid_channels)
+        average_spike_rates = raw_data_spike_rates
+
+    print('--------------------combined--------------------')
     print(average_spike_rates)
 
     return average_spike_rates
 
-def read_sorted_data(round_path, sorted_spikes_filename ='sorted_spikes.pkl', compiled_trials_filename ='compiled.pk1'):
+
+def read_sorted_data(round_path, sorted_spikes_filename='sorted_spikes.pkl', compiled_trials_filename='compiled.pk1'):
     compiled_trials_filepath = os.path.join(round_path, compiled_trials_filename)
     raw_trial_data = pd.read_pickle(compiled_trials_filepath).reset_index(drop=True)
     rhd_file_path = os.path.join(round_path, 'info.rhd')
@@ -51,6 +53,8 @@ def read_sorted_data(round_path, sorted_spikes_filename ='sorted_spikes.pkl', co
     sample_rate = load_intan_rhd_format.read_data(rhd_file_path)["frequency_parameters"]['amplifier_sample_rate']
     sorted_data = calculate_spike_timestamps(raw_trial_data, sorted_spikes, sample_rate)
     return sorted_data
+
+
 def compute_spike_rates_per_channel_per_monkey_for_all_channels(raw_trial_data):
     # average spike rates for each monkey
     unique_monkeys = raw_trial_data['MonkeyName'].dropna().unique().tolist()
@@ -81,6 +85,7 @@ def compute_spike_rates_per_channel_per_monkey_for_all_channels(raw_trial_data):
     print(avg_spike_rate_by_unit)
     return avg_spike_rate_by_unit
 
+
 def compute_spike_rates_per_channel_per_monkey(raw_trial_data, valid_channels):
     # average spike rates for each monkey
     unique_monkeys = raw_trial_data['MonkeyName'].dropna().unique().tolist()
@@ -110,6 +115,7 @@ def compute_spike_rates_per_channel_per_monkey(raw_trial_data, valid_channels):
     print(avg_spike_rate_by_unit)
     return avg_spike_rate_by_unit
 
+
 def set_node_attributes_with_default(graph, values_dict, attribute_name, default_value=0):
     for node in graph.nodes():
         value = values_dict.get(node, default_value)
@@ -118,12 +124,8 @@ def set_node_attributes_with_default(graph, values_dict, attribute_name, default
 
 if __name__ == '__main__':
     date = "2023-10-27"
-    # round = "1698696277254800_231030_160438"
-    # round = "1698699440778381_231030_165721"
-    round = "1698427250062585_231027_132051"
-    avg_spike_rates = compute_average_spike_rates(date, round)
-
-
+    round_number = 1
+    avg_spike_rates = compute_average_spike_rates(date, round_number)
 
 # spike rate for each picture
 # for index, row in raw_trial_data.iterrows():
