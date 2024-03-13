945c22f10d7ffcf9f381f4c0944d602f514cd663
Julie <hwangjy90@gmail.com>
2024-03-13 18:37:55 -0400

    in the process of simplifying single_dimensional_analysis.py

--- src/analyses/single_dimensional_analysis.py
+++ src/analyses/single_dimensional_analysis.py
@@ -1,31 +1,20 @@
 import os
 import matplotlib.pyplot as plt
+
+import spike_rate_analysis
 from monkey_names import Monkey as M
 from pathlib import Path
 
 from social_data_reader import read_raw_social_data, clean_raw_social_data, extract_specific_interaction_type, \
-    generate_edgelist_from_pairwise_interactions
-from spike_rate_analysis import read_sorted_data, compute_spike_rates_per_channel_per_monkey_for_sorted_data
+    generate_edgelist_from_pairwise_interactions, read_social_data_and_validate
 
-current_dir = os.getcwd()
-raw_data_file_name = '../../resources/ZombiesFinalRawData.xlsx'
-file_path = Path(current_dir).parent.parent.parent / 'resources' / raw_data_file_name
-raw_social_data = read_raw_social_data(file_path)
-social_data = clean_raw_social_data(raw_social_data)
+social_data = read_social_data_and_validate()
 
 affiliative = extract_specific_interaction_type(social_data, 'affiliative')
 edgelist_affiliative = generate_edgelist_from_pairwise_interactions(affiliative)
 print(f'edgelist: {edgelist_affiliative}')
-date = "2023-10-30"
-round = "1698699440778381_231030_165721"
-cortana_path = "/home/connorlab/Documents/IntanData/Cortana"
-round_path = os.path.join(cortana_path, date, round)
-raw_trial_data = read_sorted_data(round_path)
-avg_spike_rate = compute_spike_rates_per_channel_per_monkey_for_sorted_data(raw_trial_data)
+avg_spike_rate = spike_rate_analysis.compute_overall_average_spike_rates_for_each_round("2023-09-29", 2)
 print(f'avg_spike_rate: {avg_spike_rate}')
-random_row = avg_spike_rate.loc["Channel.C_017_Unit 1"]
-norm_values = ((random_row - random_row.min()) / (random_row.max() - random_row.min())).to_dict()
-print(norm_values)
 
 # Get all zombies
 zombies = [member.value for name, member in M.__members__.items() if name.startswith('Z_')]
@@ -39,7 +28,7 @@ print(sorted)
 # actor
 for zombie in zombies:
     actor_specific = sorted[sorted['Focal Name'] == zombie].copy()
-    actor_specific['Spike Rate'] = actor_specific['Social Modifier'].map(norm_values)
+    actor_specific['Spike Rate'] = actor_specific['Social Modifier'].map(avg_spike_rate)
 
     # Remove 81G spike rate since it's nan
     actor_specific_cleaned = actor_specific.dropna(subset=['Spike Rate'])
@@ -51,19 +40,19 @@ for zombie in zombies:
     plt.ylabel('Firing Rate')
     plt.title(f'affiliative behavior from {zombie}')
     plt.show()
-
+#
 # receiver
-for zombie in zombies:
-    receiver_specific = sorted[sorted['Social Modifier'] == zombie].copy()
-    receiver_specific['Spike Rate'] = receiver_specific['Focal Name'].map(norm_values)
-
-    # Remove 81G spike rate since it's nan
-    receiver_specific_cleaned = receiver_specific.dropna(subset=['Spike Rate'])
-
-    plt.plot(receiver_specific_cleaned['weight'], receiver_specific_cleaned['Spike Rate'], marker='o', linestyle='None')
-    for index, row in receiver_specific_cleaned.iterrows():
-       plt.text(row['weight'], row['Spike Rate'], row['Focal Name'], fontsize=12, ha='right', va='bottom')
-    plt.xlabel('Frequency of Affiliative Behavior')
-    plt.ylabel('Firing Rate')
-    plt.title(f'affiliative behavior to {zombie}')
-    plt.show()
+# for zombie in zombies:
+#     receiver_specific = sorted[sorted['Social Modifier'] == zombie].copy()
+#     receiver_specific['Spike Rate'] = receiver_specific['Focal Name'].map(norm_values)
+#
+#     # Remove 81G spike rate since it's nan
+#     receiver_specific_cleaned = receiver_specific.dropna(subset=['Spike Rate'])
+#
+#     plt.plot(receiver_specific_cleaned['weight'], receiver_specific_cleaned['Spike Rate'], marker='o', linestyle='None')
+#     for index, row in receiver_specific_cleaned.iterrows():
+#        plt.text(row['weight'], row['Spike Rate'], row['Focal Name'], fontsize=12, ha='right', va='bottom')
+#     plt.xlabel('Frequency of Affiliative Behavior')
+#     plt.ylabel('Firing Rate')
+#     plt.title(f'affiliative behavior to {zombie}')
+#     plt.show()
