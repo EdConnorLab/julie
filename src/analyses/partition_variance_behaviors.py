845d3f36ee42cb5d289d667979b8824f6acead08
Julie <hwangjy90@gmail.com>
2024-03-27 17:19:59 -0400

    partition variance calculation commented for clarity

--- src/analyses/partition_variance_behaviors.py
+++ src/analyses/partition_variance_behaviors.py
@@ -11,20 +11,17 @@ from monkey_names import Monkey
 excel_data_reader = ExcelDataReader(file_name='feature_df_submissive.xlsx')
 beh = excel_data_reader.get_first_sheet()
 
-Sm_arrow = beh.sum(axis=1) / (beh.shape[1] - 1)
-Sarrow_m = beh.sum(axis=0) / (beh.shape[0] - 1)
+Sm_arrow = beh.sum(axis=1) / (beh.shape[1] - 1)  # average frequency of the monkey m submitting to other monkeys
+Sarrow_m = beh.sum(axis=0) / (beh.shape[0] - 1)  # average frequency of other monkeys submitting to the monkey m
 
 sum_Sm_arrow = Sm_arrow.sum()  # this should be the same as sum_Sarrow_m = Sarrow_m.sum()
 
 Sm_arrow_values = Sm_arrow.values.reshape(-1, 1)
 Sarrow_m_values = Sarrow_m.values.reshape(-1, 1)
 
-Sm_arrow_n = Sm_arrow_values * Sarrow_m_values.T
-
-temp = Sm_arrow_n / sum_Sm_arrow
-final = beh.values - temp
-np.fill_diagonal(final, 0)
-beh_final = pd.DataFrame(final, columns=beh.columns)
+RSm_n = beh.values - ((Sm_arrow_values * Sarrow_m_values.T) / sum_Sm_arrow)
+np.fill_diagonal(RSm_n, 0)
+beh_final = pd.DataFrame(RSm_n, columns=beh.columns)
 print(beh_final)  # this table is RSm->n
 
 # Get 81G
