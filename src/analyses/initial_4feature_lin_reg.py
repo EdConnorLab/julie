62c24fcb289fc2e6a118e8c996c3f10b2e667262
Julie <hwangjy90@gmail.com>
2024-04-17 21:19:57 -0400

    creating r-squared hitograms

--- src/analyses/initial_4feature_lin_reg.py
+++ src/analyses/initial_4feature_lin_reg.py
@@ -25,9 +25,14 @@ def construct_feature_matrix(beh, Sm_arrow, Sarrow_m, behavior_type):
     feature_matrix = np.delete(feat.values, 6, axis=0)  # delete information on 81G
     return feature_matrix, feature_names
 
+def get_metadata_for_regression():
+    metadata_reader = RecordingMetadataReader()
+    raw_metadata = metadata_reader.get_raw_data()
+    metadata_for_regression = raw_metadata.parse('InitialRegression')
 
+    return metadata_for_regression
 
-def run_single_feature_linear_regression_analysis(X, feature_names, behavior_type):
+def run_single_feature_linear_regression_analysis(X, metadata_for_regression, feature_names, behavior_type):
     # SINGLE FEATURE
     all_results = []
     cell_count = 0
@@ -41,7 +46,7 @@ def run_single_feature_linear_regression_analysis(X, feature_names, behavior_typ
         for ind, zombies_row in spike_rates_zombies.iterrows():
             print(f"{date} round {round_no} : performing linear regression for {ind}")
             Y = np.array(zombies_row.values).reshape(-1, 1)
-            fig = plt.figure(figsize=(12, 14))
+            fig = plt.figure(figsize=(14, 14))
             fig.suptitle(f"{date} Round #{round_no}: {ind}")
             cell_count += 1
             should_save_fig = False  # Initialize the flag
@@ -79,27 +84,77 @@ def run_single_feature_linear_regression_analysis(X, feature_names, behavior_typ
     # final_df.to_excel(f'/home/connorlab/Documents/GitHub/Julie/linear_regression_results/single_feature_{behavior_type}.xlsx', index=False)
     print(f'cell count: {cell_count}')
 
-''' 
 
-Looking at each neuron using average spikes rate over 10 trials
-1D analysis for different types of behaviors (affiliation, submission, aggression)
 
-'''
-# Get all experimental round information
-zombies = [member.value for name, member in Monkey.__members__.items() if name.startswith('Z_')]
-metadata_reader = RecordingMetadataReader()
-raw_metadata = metadata_reader.get_raw_data()
-metadata_for_regression = raw_metadata.parse('InitialRegression')
 
-agon_beh, Sm_arrow_agon, Sarrow_m_agon = social_data_processor.partition_behavior_variance_from_excel_file('feature_df_agonism.xlsx')
-sub_beh, Sm_arrow_sub, Sarrow_m_sub = social_data_processor.partition_behavior_variance_from_excel_file('feature_df_submission.xlsx')
-aff_beh, Sm_arrow_aff, Sarrow_m_aff = social_data_processor.partition_behavior_variance_from_excel_file('feature_df_affiliation.xlsx')
 
-X_agon, agon_feature_names = construct_feature_matrix(agon_beh, Sm_arrow_agon, Sarrow_m_agon, 'agonism')
-X_sub, sub_feature_names = construct_feature_matrix(sub_beh, Sm_arrow_sub, Sarrow_m_sub, 'submission')
-X_aff, aff_feature_names = construct_feature_matrix(aff_beh, Sm_arrow_aff, Sarrow_m_aff, 'affiliation')
+def generate_r_squared_histogram_for_specific_population(X, feature_names, location):
+    metadata = get_metadata_for_regression()
+    neural_population = metadata[metadata['Location'] == location]
+    stat_param_list = []
+    for index, row in neural_population.iterrows():
+        date = row['Date'].strftime('%Y-%m-%d')
+        round_no = row['Round No.']
+        spike_rates = spike_rate_analysis.get_average_spike_rates_for_each_monkey(date, round_no)
+        spike_rates_zombies = spike_rates[[col for col in zombies if col in spike_rates.columns]]
 
-run_single_feature_linear_regression_analysis(X_agon, agon_feature_names, 'agonism')
-run_single_feature_linear_regression_analysis(X_sub, sub_feature_names, 'submission')
-run_single_feature_linear_regression_analysis(X_aff, aff_feature_names, 'affiliation')
+        for ind, zombies_row in spike_rates_zombies.iterrows():
+            print(f"{date} round {round_no} : performing linear regression for {ind}")
+            Y = np.array(zombies_row.values).reshape(-1, 1)
+
+            for col_index in range(X.shape[1]):
+                column = X[:, col_index].reshape(-1, 1)
+                augmented_X = np.hstack((column, np.ones((column.shape[0], 1))))
+                model = OLS(Y, augmented_X)
+                results = model.fit()
+                stat_params_to_be_saved = [date, round_no, ind, feature_names[col_index], results.rsquared,
+                                           results.pvalues[0], results.params]
+                stat_param_list.append(stat_params_to_be_saved)
+    results_df = pd.DataFrame(stat_param_list, columns=['Date', 'Round', 'Neuron', 'Feature Name', 'R-squared',
+                                                        'p-value', 'coefficients'])
+
+    return results_df
+
+
+if __name__ == '__main__':
+    ''' 
+
+    Looking at each neuron using average spikes rate over 10 trials
+    1D analysis for different types of behaviors (affiliation, submission, aggression)
+
+    '''
+    # Get all experimental round information
+    zombies = [member.value for name, member in Monkey.__members__.items() if name.startswith('Z_')]
+    metadata_for_regression = get_metadata_for_regression()
+
+    agon_beh, Sm_arrow_agon, Sarrow_m_agon = social_data_processor.partition_behavior_variance_from_excel_file(
+        'feature_df_agonism.xlsx')
+    sub_beh, Sm_arrow_sub, Sarrow_m_sub = social_data_processor.partition_behavior_variance_from_excel_file(
+        'feature_df_submission.xlsx')
+    aff_beh, Sm_arrow_aff, Sarrow_m_aff = social_data_processor.partition_behavior_variance_from_excel_file(
+        'feature_df_affiliation.xlsx')
+
+    X_agon, agon_feature_names = construct_feature_matrix(agon_beh, Sm_arrow_agon, Sarrow_m_agon, 'agonism')
+    X_sub, sub_feature_names = construct_feature_matrix(sub_beh, Sm_arrow_sub, Sarrow_m_sub, 'submission')
+    X_aff, aff_feature_names = construct_feature_matrix(aff_beh, Sm_arrow_aff, Sarrow_m_aff, 'affiliation')
+
+    # run_single_feature_linear_regression_analysis(X_agon, metadata_for_regression, agon_feature_names, 'agonism')
+    # run_single_feature_linear_regression_analysis(X_sub, metadata_for_regression, sub_feature_names, 'submission')
+    # run_single_feature_linear_regression_analysis(X_aff, metadata_for_regression, aff_feature_names, 'affiliation')
+
+    agon_df = generate_r_squared_histogram_for_specific_population(X_agon, agon_feature_names, 'ER')
+    agon_sorted = agon_df.sort_values(by='R-squared', ascending=False)
+
+    # Filter and plot
+    for feature_name, group in agon_sorted.groupby('Feature Name'):
+        filtered_group = group[group['R-squared'] > 0.1]
+
+        # Check if there are any values to plot
+        if not filtered_group.empty:
+            plt.figure()
+            filtered_group['R-squared'].hist(bins=20)  # You can adjust the number of bins
+            plt.title(f'Histogram of r-squared values for {feature_name}')
+            plt.xlabel('R-squared')
+            plt.ylabel('Number of Cells')
+            plt.show()
 
