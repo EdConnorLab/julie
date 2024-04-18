abde752f6431f780fd2112c1828648dfce6dd8db
Julie <hwangjy90@gmail.com>
2024-04-17 20:36:34 -0400

    plots for single_feature_plots

--- src/analyses/initial_4feature_lin_reg.py
+++ src/analyses/initial_4feature_lin_reg.py
@@ -30,9 +30,11 @@ def construct_feature_matrix(beh, Sm_arrow, Sarrow_m, behavior_type):
 def run_single_feature_linear_regression_analysis(X, feature_names, behavior_type):
     # SINGLE FEATURE
     all_results = []
+    cell_count = 0
     for index, row in metadata_for_regression.iterrows():
         date = row['Date'].strftime('%Y-%m-%d')
         round_no = row['Round No.']
+        location = row['Location']
         spike_rates = spike_rate_analysis.get_average_spike_rates_for_each_monkey(date, round_no)
         spike_rates_zombies = spike_rates[[col for col in zombies if col in spike_rates.columns]]
         labels = spike_rates_zombies.columns.tolist()
@@ -41,7 +43,7 @@ def run_single_feature_linear_regression_analysis(X, feature_names, behavior_typ
             Y = np.array(zombies_row.values).reshape(-1, 1)
             fig = plt.figure(figsize=(12, 14))
             fig.suptitle(f"{date} Round #{round_no}: {ind}")
-
+            cell_count += 1
             should_save_fig = False  # Initialize the flag
 
             for col_index in range(X.shape[1]):
@@ -62,19 +64,20 @@ def run_single_feature_linear_regression_analysis(X, feature_names, behavior_typ
                 ax.set_ylabel(f"Average Firing Rate")
                 ax.set_title(f"R-squared: {results.rsquared:.2f}, p-value {results.pvalues[0]:.6f}")
                 # print(results.summary())
-                if (results.rsquared > 0.7) and (results.pvalues[0] < 0.05):
-                    should_save_fig = True
-                    stat_params_to_be_saved = [date, round_no, ind, feature_names[col_index], results.rsquared,
-                                               results.pvalues[0], results.params]
-                    all_results.append(stat_params_to_be_saved)
-            if should_save_fig:
-                fig.savefig(f'/home/connorlab/Documents/GitHub/Julie/linear_regression_results/'
-                                f'single_feature_plots/{behavior_type}_{date}_round{round_no}_{ind}.png')
+            #     if True:  # (results.rsquared > 0.7) and (results.pvalues[0] < 0.05):
+            #         should_save_fig = True
+            #         stat_params_to_be_saved = [date, round_no, ind, feature_names[col_index], results.rsquared,
+            #                                    results.pvalues[0], results.params]
+            #         all_results.append(stat_params_to_be_saved)
+            #
+            # if should_save_fig:
+            #     fig.savefig(f'/home/connorlab/Documents/GitHub/Julie/linear_regression_results/'
+            #                     f'single_feature_plots/{behavior_type}_{date}_round{round_no}_{location}_{ind}.png')
                 # plt.show()
     # final_df = pd.DataFrame(all_results, columns=['Date', 'Round', 'Neuron', 'Feature Name', 'R-squared',
     #                                              'p-value', 'coefficients'])
     # final_df.to_excel(f'/home/connorlab/Documents/GitHub/Julie/linear_regression_results/single_feature_{behavior_type}.xlsx', index=False)
-
+    print(f'cell count: {cell_count}')
 
 ''' 
 
