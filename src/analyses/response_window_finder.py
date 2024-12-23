c4ff07c4a5de45477b509d42456ac24206fca2d5
Julie <hwangjy90@gmail.com>
2024-12-23 18:36:23 -0500

    trying to get the window from finding response window with manual threshold

--- src/analyses/response_window_finder.py
+++ src/analyses/response_window_finder.py
@@ -17,10 +17,28 @@ def sum_lists(row):
 def fractional_change(lst):
     return [(lst[i+1] - lst[i]) / lst[i] if lst[i] != 0 else 0 for i in range(len(lst)-1)]
 
+def calculate_fractional_change(data, monkeys, channels, chunk_size):
+    spike_counts = get_spike_counts_for_time_chunks(monkeys, data, channels, chunk_size)
+    spike_counts['total_sum'] = spike_counts.apply(lambda row: sum_lists(row), axis=1)
+    spike_counts['fractional_change'] = spike_counts['total_sum'].apply(fractional_change)
+    return  spike_counts[['total_sum', 'fractional_change']]
+
+
+def plot_fractional_change_data(date, round_no, data, time_chunk_size):
+    max_length = data['fractional_change'].apply(len).max()
+    time_in_sec = np.arange(time_chunk_size, time_chunk_size * (max_length+1), time_chunk_size)
+    for index, row in data.iterrows():
+        plt.plot(time_in_sec, row['fractional_change'], label=index)
+    plt.title(f'{date} Round {round_no} Fractional Change by Channel')
+    plt.legend()
+    plt.yticks(np.arange(min(plt.ylim()), max(plt.ylim()), 0.5))
+    plt.grid(axis='y', linestyle='--', linewidth=0.5)
+    plt.axhline(y=1.8, color='black') # threshold
+    plt.show()
+
 
 
 if __name__ == '__main__':
-
 
     time_windows = generate_time_windows_for_given_window_size(50)
 
@@ -28,36 +46,52 @@ if __name__ == '__main__':
     del zombies[6]
     del zombies[-1]
 
-    date = "2023-10-04"
-    round_no = 1
+    date = "2023-10-27"
+    round_no = 2
     raw_unsorted_data, valid_channels, sorted_data = get_raw_data_and_channels_from_files(date, round_no)
 
-    chunk_size = 0.05
-    spike_counts_for_unsorted = get_spike_counts_for_time_chunks(zombies, raw_unsorted_data, valid_channels, chunk_size)
-    spike_counts_for_unsorted['total_sum'] = spike_counts_for_unsorted.apply(lambda row: sum_lists(row), axis=1)
-    spike_counts_for_unsorted['fractional_change'] = spike_counts_for_unsorted['total_sum'].apply(fractional_change)
-    print(spike_counts_for_unsorted[['total_sum', 'fractional_change']])
-    subset_df = spike_counts_for_unsorted[['total_sum', 'fractional_change']]
-    for index, row in subset_df.iterrows():
-        plt.plot(row['fractional_change'], label=index)
-    plt.title('Fractional Change by Channel')
-    plt.legend()
-    plt.show()
+    time_chunk_size = 0.2  # in sec
+    unsorted_df = calculate_fractional_change(raw_unsorted_data, zombies, valid_channels, time_chunk_size)
+    # plot_fractional_change_data(date, round_no, unsorted_df, time_chunk_size)
 
     if sorted_data is not None:
         unique_channels = set()
         unique_channels.update(sorted_data['SpikeTimes'][0].keys())
-        spike_counts_for_sorted = get_spike_counts_for_time_chunks(zombies, sorted_data, unique_channels, chunk_size)
-        spike_counts_for_sorted['total_sum'] = spike_counts_for_sorted.apply(lambda row: sum_lists(row), axis=1)
-        spike_counts_for_sorted['fractional_change'] = spike_counts_for_sorted['total_sum'].apply(fractional_change)
-        print(spike_counts_for_sorted[['total_sum', 'fractional_change']])
-        subset_sorted = spike_counts_for_sorted[['total_sum', 'fractional_change']]
-        for index, row in subset_sorted.iterrows():
-            plt.plot(row['fractional_change'], label = index)
-        plt.title('Fractional Change by Channel')
-        plt.legend()
+        sorted_df = calculate_fractional_change(sorted_data, zombies, unique_channels, time_chunk_size)
+        # plot_fractional_change_data(date, round_no, sorted_df, time_chunk_size)
+
+    max_length = unsorted_df['fractional_change'].apply(len).max()
+    time_in_sec = np.arange(time_chunk_size, time_chunk_size * (max_length+1), time_chunk_size)
+    for index, row in unsorted_df.iterrows():
+        threshold = 1.8
+        row_array = np.array(row['fractional_change'])
+        above_threshold = row_array > threshold
+        crossings = np.diff(above_threshold.astype(int))
+        crossing_up_indices = np.where(crossings == 1)[0]
+        crossing_down_indices = np.where(crossings == -1)[0] + 1
+        plt.plot(time_in_sec, row_array)
+
+        # Ensure the index does not exceed the array bounds
+        if crossing_down_indices[-1] >= len(row_array):
+            crossing_down_indices = crossing_down_indices[:-1]
+
+        if crossing_up_indices.size > 0 and crossing_down_indices.size > 0:
+            if crossing_up_indices[0] > crossing_down_indices[0]:
+                crossing_down_indices = crossing_down_indices[1:]
+            start_index = crossing_up_indices[0]
+            end_index = crossing_down_indices[0] if crossing_down_indices.size > 0 else -1
+
+            start_point = (time_in_sec[start_index], row_array[start_index])
+            end_point = (time_in_sec[end_index], row_array[end_index])
+            plt.axhline(y=threshold, color='r', linestyle='--', label='Threshold')
+            plt.plot(time_in_sec[start_index], row_array[start_index], 'go', label='Start of Window')  # Green dot
+            plt.plot(time_in_sec[end_index], row_array[end_index], 'mo', label='End of Window')  # Magenta dot
+            plt.yticks(np.arange(min(plt.ylim()), max(plt.ylim()), 0.5))
+            plt.grid(axis='y', linestyle='--', linewidth=0.5)
+            plt.title(f'{index}')
         plt.show()
+
     # results_df_final = drop_duplicate_channels_with_matching_time_window(all_anova_sig_results)
-    # print(results_df_final)
-    # results_df_final.to_excel('/home/connorlab/Documents/GitHub/Julie/anova_scan/ER_ANOVA_scan_size_300ms.xlsx')
+
+    # set threshold to 1.8?
 
